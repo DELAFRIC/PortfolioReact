7b2a0a8c5a71b1707cb953ac0fd86c9c189f0f16
DELAFRIC <ogouachimax@gmail.com>
2025-04-22 03:45:40 +0000

    troisieme commit

--- src/acceuil/pageAcceuil1.jsx
+++ src/acceuil/pageAcceuil1.jsx
@@ -1,27 +1,27 @@
  import './pageAcceuil1.css'
-import pto from '../assets/pto.gif'
+import kevyn from '../assets/kevyn.webp'
 
 
  function CadreCouleur(){
     return <div className = "grandCarde" >
-            <div className="parent">
+            <div className="conteneur">
                 <div className='row'>
                         <div className='col-8'>
                             <div>
-                                <div><h1> Mr.Delafric</h1></div>
-                                <div><h2> Igenieur logiciel</h2></div>
-                                <div><h3> consectetur adipisicing elit. Repellat  eligendi, autem illum odi</h3></div>
-                                <div className='row'>
-                                    <div className='col-6'><p>Téléchargement Cv</p></div>
-                                    <div className='col-2'><p>wat</p></div>
-                                    <div className='col-2'><p>tiktok</p></div>
-                                    <div className='col-2'><p>Fb</p></div>
-                                </div>
+                                <div className='Mr'><h1> Mr.Delafric</h1></div>
+                                <div className='titre'><h2> Ingenieur logiciel</h2></div>
+                                <div className='slogan'><h3> consectetur adipisicing elit. Repellat  eligendi, autem illum odi Repellat  eligendi, autem illum odi</h3></div>
+                                <ul className='row conteneurCV'>
+                                    <li className='col-6 cv'>Téléchargement Cv</li>
+                                    <li className='col-2 wat'><i className="bi bi-whatsapp"></i></li>
+                                    <li className='col-2 tiktok'><i className="bi bi-tiktok"></i></li>
+                                    <li className='col-2 fb'><i className="bi bi-facebook"></i></li>
+                                </ul>
                             </div>
                         </div>
-                        <div className='col-4'>
+                        <div className='col-4 imagekevyn'>
                             <div>
-                                <img src={pto} alt="image delafric" style ={{width:"70%"}}/>
+                                <img src={kevyn} alt="image Kevyn"/>
                             </div>
                         </div>
                 </div>
@@ -32,16 +32,13 @@ import pto from '../assets/pto.gif'
 
  function Liste(){
     return <div>
-        <div className='listParent'>
-            <div>
-                <ul>
-                    <li>A propos</li>
-                    <li>Services</li>
-                    <li>Projets</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-           
+        <div className='listParent'>          
+                <ul className='row'>
+                    <li className='col-3'>À propos</li>
+                    <li className='col-3'>Services</li>
+                    <li className='col-3'>Projets</li>
+                    <li className='col-3'>Contact</li>
+                </ul>   
         </div>
     </div>
  }
@@ -60,9 +57,6 @@ import pto from '../assets/pto.gif'
 
 export default function AppPageAcceuil1(){
     return <>
-        {/* <div> */}
             <Sorti/>
-        {/* </div> */}
-
     </>
 }
