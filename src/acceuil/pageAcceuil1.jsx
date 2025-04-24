51a8bdaec20fccfdc2461336284511bdbf6b89f0
delafric <ogouachimax@gmail.com>
2025-04-24 05:08:58 +0200

    commit quatre

--- src/acceuil/pageAcceuil1.jsx
+++ src/acceuil/pageAcceuil1.jsx
@@ -7,22 +7,23 @@ import kevyn from '../assets/kevyn.webp'
             <div className="conteneur">
                 <div className='row'>
                         <div className='col-8'>
-                            <div>
+                            <div className='tout'>
                                 <div className='Mr'><h1> Mr.Delafric</h1></div>
                                 <div className='titre'><h2> Ingenieur logiciel</h2></div>
                                 <div className='slogan'><h3> consectetur adipisicing elit. Repellat  eligendi, autem illum odi Repellat  eligendi, autem illum odi</h3></div>
                                 <ul className='row conteneurCV'>
                                     <li className='col-6 cv'>Téléchargement Cv</li>
-                                    <li className='col-2 wat'><i className="bi bi-whatsapp"></i></li>
-                                    <li className='col-2 tiktok'><i className="bi bi-tiktok"></i></li>
-                                    <li className='col-2 fb'><i className="bi bi-facebook"></i></li>
+                                    <div className='row col-6 conteneurReseau'>
+                                        <li className='col-4 wat'><i className="bi bi-whatsapp"></i></li>
+                                        <li className='col-4 tiktok'><i className="bi bi-tiktok"></i></li>
+                                        <li className='col-4 fb'><i className="bi bi-facebook"></i></li>
+                                    </div>
                                 </ul>
                             </div>
                         </div>
                         <div className='col-4 imagekevyn'>
-                            <div>
                                 <img src={kevyn} alt="image Kevyn"/>
-                            </div>
+
                         </div>
                 </div>
             </div>
@@ -32,7 +33,7 @@ import kevyn from '../assets/kevyn.webp'
 
  function Liste(){
     return <div>
-        <div className='listParent'>          
+        <div className='listParent '>          
                 <ul className='row'>
                     <li className='col-3'>À propos</li>
                     <li className='col-3'>Services</li>
@@ -40,6 +41,21 @@ import kevyn from '../assets/kevyn.webp'
                     <li className='col-3'>Contact</li>
                 </ul>   
         </div>
+        {/* <nav className="navbar navbar-expand-md-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className=" row navbar-nav ">
+                        <li className='col-3'>À propos</li>
+                        <li className='col-3'>Services</li>
+                        <li className='col-3'>Projets</li>
+                        <li className='col-3'>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </nav> */}
     </div>
  }
 
